1972007447bc4359a904604bb81a8372dca3cb30
prime156 <prime156@MacBookP156.local>
2018-07-22 22:16:03 +0900

    second

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ function readCondition() {
 
    result.legs_type = document.querySelector('#legs_type').value;
 
-   var keys = 'ap,ke,ce,te,en,weight_cap,legs_move,legs_stab,legs_turn,head_stab,head_cam,head_scan,arms_stab,arms_cap,core_supp'.split(',');
+   var keys = 'ap,ke,ce,te,en,weight_cap,stab,legs_move,legs_stab,legs_turn,head_stab,head_cam,head_scan,arms_stab,arms_cap,core_supp'.split(',');
 
    var int = function(str) {
       return parseInt(str, 10);
@@ -67,7 +67,7 @@ function loadCondition() {
       document.querySelector('#legs_type').selectedIndex = legsTypes[condition.legs_type] | 0;
    };
 
-   var keys = 'ap,ke,ce,te,en,weight_cap,legs_move,legs_stab,legs_turn,head_stab,head_cam,head_scan,arms_stab,arms_cap,core_supp'.split(',');
+   var keys = 'ap,ke,ce,te,en,weight_cap,stab,legs_move,legs_stab,legs_turn,head_stab,head_cam,head_scan,arms_stab,arms_cap,core_supp'.split(',');
 
    for (var i = 0, n = keys.length; i < n; i++) {
       if (condition[keys[i] + '_min'] != null) {
@@ -78,15 +78,12 @@ function loadCondition() {
          condition[keys[i] + '_max'] === 99999 ? set(keys[i] + '_max', '') : set(keys[i] + '_max', condition[keys[i] + '_max']);
       }
    }
-
 };
 
 function search() {
    saveCondition()
 
    var condition = readCondition();
-
-   console.log('search:: condition:', condition);
 
    var int = function(str) {
       return parseInt(str, 10);
@@ -96,7 +93,16 @@ function search() {
       return int(head[param]) + int(core[param]) + int(arms[param]) + int(legs[param]);
    };
 
-   var label = 'HEAD,CORE,ARMS,LEGS,AP,KE防御,CE防御,TE防御,消費EN,重量,余剰積載,射撃安定,格納,移動制御,姿勢制御,旋回性能,安定演算,カメラ,スキャン'.split(',');
+   var calcStab = function(head, legs) {
+      // (352 * 安定演算/1000 * 姿勢制御/1000 + 320) * (1 + 総重量/20000)
+      var h = head['安定演算性能'] / 1000;
+      var l = legs['姿勢制御'] / 1000;
+      var w = 1 + (int(legs['重量']) + int(legs['積載量'])) / 20000;
+
+      return Math.round((352 * h * l + 320) * w);
+   };
+
+   var label = 'HEAD,CORE,ARMS,LEGS,AP,KE防御,CE防御,TE防御,消費EN,重量,余剰積載,対反上限,射撃安定,格納,移動制御,姿勢制御,旋回性能,安定演算,カメラ,スキャン,EN伝達'.split(',');
    var frame = [];
 
    for (var ih = 0, nh = parts.HEAD.length; ih < nh; ih++) {
@@ -125,6 +131,9 @@ function search() {
                if (l['姿勢制御'] < condition.legs_stab_min || condition.legs_stab_max < l['姿勢制御']) continue;
                if (l['旋回性能'] < condition.legs_turn_min || condition.legs_turn_max < l['旋回性能']) continue;
 
+               var stab = calcStab(h, l);
+               if (stab < condition.stab_min || condition.stab_max < stab) continue;
+
                _tmp = [
                   h['名称'], c['名称'], a['名称'], l['名称'],
                   sum(h, c, a, l, 'AP'),
@@ -136,6 +145,7 @@ function search() {
                   l['積載量'] - sum(h, c, a, {
                      '重量': 0
                   }, '重量'),
+                  stab,
                   a['射撃安定性能'],
                   a['格納数'],
                   l['移動制御'],
@@ -143,7 +153,8 @@ function search() {
                   l['旋回性能'],
                   h['安定演算性能'],
                   h['カメラ性能'],
-                  h['スキャン性能']
+                  h['スキャン性能'],
+                  c['EN伝達']
                ];
 
                if (_tmp[4] < condition.ap_min || condition.ap_max < _tmp[4]) continue;
@@ -152,8 +163,6 @@ function search() {
                if (_tmp[7] < condition.te_min || condition.te_max < _tmp[7]) continue;
                if (_tmp[8] < condition.en_min || condition.en_max < _tmp[8]) continue;
                if (_tmp[10] < condition.weight_cap_min || condition.weight_cap_max < _tmp[10]) continue;
-
-               // console.log(_tmp[10], condition.weihgt_cap_min, condition.weihgt_cap_max);
 
                frame.push(_tmp);
             }
@@ -195,3 +204,22 @@ function genTable(label, data) {
 
    document.querySelector('#table').innerHTML = html;
 }
+
+function toggleDatalist(type) {
+   var map = {
+      '軽二': '2LEGS-L',
+      '中二': '2LEGS-M',
+      '重二': '2LEGS-H',
+      '軽逆': 'REVERSE-JOIN-L',
+      '重逆': 'REVERSE-JOIN-H',
+      '四脚': '4LEGS',
+      'タンク': 'TANK'
+   };
+
+   var keys = 'legs_move,legs_stab,legs_turn'.split(',');
+
+   for (var i = 0, n = keys.length; i < n; i++) {
+      document.querySelector('#' + keys[i] + '_min').setAttribute('list', 'list-' + keys[i] + '-' + map[type]);
+      document.querySelector('#' + keys[i] + '_max').setAttribute('list', 'list-' + keys[i] + '-' + map[type]);
+   }
+};
